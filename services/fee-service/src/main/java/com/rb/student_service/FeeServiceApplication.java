c195457d4d4fe8816c693dcbed272ce2e5b62f88
Soumitra Soundankar <soumitra.soundankar@gmail.com>
2024-09-13 18:54:18 +0530

    Added fee-service

---
+++ services/fee-service/src/main/java/com/rb/student_service/FeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rb.student_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApplication.class, args);
+	}
+
+}
