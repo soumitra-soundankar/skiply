e1d89a672fc31b7988c328e3ee9e9f5a9b81d9f2
Soumitra Soundankar <soumitra.soundankar@gmail.com>
2024-09-13 19:04:31 +0530

    Added payment-service

--- services/fee-service/src/test/java/com/rb/fee_service/StudentServiceApplicationTests.java
+++ services/fee-service/src/test/java/com/rb/fee_service/StudentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rb.student_service;
+package com.rb.fee_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- services/payment-service/src/main/java/com/rb/payment_service/PaymentServiceApplication.java
+++ services/payment-service/src/main/java/com/rb/payment_service/PaymentServiceApplication.java
@@ -1,13 +1,13 @@
-package com.rb.student_service;
+package com.rb.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FeeServiceApplication {
+public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentServiceApplication.class, args);
+		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
 
 }
